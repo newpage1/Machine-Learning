2ccef087060b1baafc9bf26ed86916239ab304c8
maorui <saberui2010@163.com>
2015-03-02 20:46:21 +0800

    bag of word

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -1,4 +1,5 @@
 from numpy import *
+from math import log
 def createlist(dataset):
     """
     select the word from the dataset one by one
@@ -18,13 +19,16 @@ def word2vec(vocablist,inputset):
     return returnvec
 
 def train(tmatrix,category):
+    """
+    0 represent the normal,and 1 represent the bad opinion
+    """
     numdocs = len(tmatrix)
     numwords = len(tmatrix[0])
     pa = sum(category)/float(numdocs)
-    p0num = zeros(numwords)
-    p1num = zeros(numwords)
-    p0 = 0.0
-    p1 = 0.0
+    p0num = ones(numwords)
+    p1num = ones(numwords)
+    p0 = 2.0
+    p1 = 2.0
     for i in range(numdocs):
 	    if category[i] == 1:
 		    p1num += tmatrix[i]
@@ -34,9 +38,34 @@ def train(tmatrix,category):
 		    p0 += sum(tmatrix[i])
     p1vec = p1num/p1
     p0vec = p0num/p0
-    return p1vec,p0vec,pa
+    p0len = len(p0vec)
+    p1len = len(p1vec)
+    for i in range(p0len):
+	    p0vec[i] = log(p0vec[i])
+    for j in range(p1len):
+	    p1vec[j] = log(p1vec[j])
+    return p0vec,p1vec,pa
+
 
 def loaddataset():
     vocablist = [['my','dog','has','flea','problems','help','please'],['maybe','not','take','him','to','dog','park','stupid'],['my','delamation','is','so','cute','i','love','him'],['stop','posting','stupid','worthless','garbage'],['mr','licks','ate','my','steak','how','to','stop','hime'],['quit','buying','worthless','dog','food','stupid']]
     classvec = [0,1,0,1,0,1]
     return vocablist,classvec
+
+def classify(veclass,p0vec,p1vec,pa):
+    p1 = sum(veclass*p1vec) + log(pa)
+    p0 = sum(veclass*p0vec) + log(1-pa)
+    if p1>p0 :
+	    return 1
+    else:
+	    return 0
+
+def bagofword(vocablist,inputset)
+    returnvec = [0]*len(vocablist)
+    for word in inputset:
+	    if word in vocablist:
+		    returnvec[vocablist.index(word)] += 1
+	    else:
+		    print "%s is not in the vocabulary" %word
+    return returnvec
+
--- bayes/simpletest.py
+++ bayes/simpletest.py
@@ -1,11 +1,21 @@
 from numpy import *
 import bayes
 def test():
+    """
+    the probability reach 1 represent badness 
+    """
     listpost,listclass = bayes.loaddataset()
     myvocablist = bayes.createlist(listpost)
     tmatrix = list()
     for doc in listpost:
 	    vec = bayes.word2vec(myvocablist,doc)
 	    tmatrix.append(vec)
-    return bayes.train(tmatrix,listclass)
-    
+    p0,p1,pa = bayes.train(tmatrix,listclass)
+    testdoc1 = ['love','my','dalmation']
+    testvec1 = bayes.word2vec(myvocablist,testdoc1)
+    print testdoc1,'classify as :',bayes.classify(testvec1,p0,p1,pa)
+    testdoc2 = ['stupid','love']
+    testvec2 = bayes.word2vec(myvocablist,testdoc2)
+    print testdoc2,'classify as :',bayes.classify(testvec2,p0,p1,pa)
+
+
