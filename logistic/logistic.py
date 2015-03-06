e65204f97561a5396fd2931b40fd408d48b848d9
maorui <saberui2010@163.com>
2015-03-06 15:03:09 +0800

    matplot

--- logistic/logistic.py
+++ logistic/logistic.py
@@ -1,5 +1,6 @@
 from math import *
 from numpy import *
+import matplotlib.pyplot as plt
 
 def sigmoid(matrix):
     m,n = shape(matrix)
@@ -31,4 +32,31 @@ def weightcount(datamat,labelmat):
 	    weights = weights + axi*(dmatrix.transpose()*error)
     return weights
 
+def plotfit():
+    data,label = loaddataset()
+    weights = weightcount(data,label)
+    darray = array(data)
+    n = darray.shape[0]
+    x1cord = list(); y1cord = list()
+    x2cord = list(); y2cord = list()
+    for i in range(n):
+	    if label[i] == 1:
+		    x1cord.append(darray[i][1])
+		    y1cord.append(darray[i][2])
+	    else:
+		    x2cord.append(darray[i][1])
+		    y2cord.append(darray[i][2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x1cord,y1cord,s = 30,c = 'red',marker = 's')
+    ax.scatter(x2cord,y2cord,s = 30,c = 'green')
+    x = arange(-3.0,3.0,0.1)
+    y = array((-weights[0]-weights[1]*x)/weights[2])
+    x = mat(x).transpose()
+    y = y.transpose()
+    ax.plot(x,y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+   # return x,y
 
